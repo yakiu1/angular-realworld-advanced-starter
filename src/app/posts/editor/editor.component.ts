9d883ef623526b91d65d63fe08398a4de6b6b978
yakiu1 <yakiu100@gmail.com>
2020-03-21 16:46:43 +0800

    Gon ReactiveForm 練習

--- src/app/posts/editor/editor.component.ts
+++ src/app/posts/editor/editor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+import { asLiteral } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-editor',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private fb: FormBuilder) { }
+  form;
   ngOnInit() {
+
+    this.form = this.fb.group(
+      {
+        title: this.fb.control('新增文章', [Validators.required]),
+        context: this.fb.control('寫入文章內容'),
+        tags: this.fb.array([this.fb.control('HTML'), this.fb.control('Angular')])
+      }
+    );
+
   }
+
+
+  public get tags(): FormArray {
+    return this.form.get('tags');
+  }
+
+
+  doAddTags(tagName: string) {
+    console.log(tagName);
+    (this.tags as FormArray)
+      .push(this.fb.control(tagName));
+  }
+
+  doRemove(index: number) {
+    this.tags.removeAt(index);
+  }
+
 
 }
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -5,12 +5,14 @@ import { PostsRoutingModule } from './posts-routing.module';
 import { EditorComponent } from './editor/editor.component';
 import { PostComponent } from './post/post.component';
 import { PostsComponent } from './posts/posts.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [EditorComponent, PostComponent, PostsComponent],
   imports: [
-    CommonModule,
-    PostsRoutingModule
+    CommonModule
+    , PostsRoutingModule
+    , ReactiveFormsModule
   ]
 })
 export class PostsModule { }
