f2f8b9becad3b627e4ccd210dc8122b7ffeaa64e
yakiu1 <yakiu100@gmail.com>
2020-03-21 12:05:21 +0800

    Gon RxJS 應用

--- src/app/posts/posts/posts.component.ts
+++ src/app/posts/posts/posts.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../post.service';
 import { Articles, Article } from '../article';
+import { Observable } from 'rxjs';
+import { map, share } from 'rxjs/operators';
 
 @Component({
   selector: 'app-posts',
@@ -9,13 +11,15 @@ import { Articles, Article } from '../article';
 })
 export class PostsComponent implements OnInit {
   articles: Article[];
+  articles$: Observable<Article[]>;
   constructor(private postService: PostService) { }
 
   ngOnInit() {
-    this.postService.getArticles().subscribe(articleData => {
-      this.articles = articleData.articles;
-      console.log(this.articles);
-    });
+    // this.postService.getArticles().subscribe(articleData => {
+    //   this.articles = articleData.articles;
+    //   console.log(this.articles);
+    // });
+    this.articles$ = this.postService.getArticles().pipe(map(m => m.articles), share());
   }
 
 }
